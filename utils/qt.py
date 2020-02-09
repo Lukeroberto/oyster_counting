5017f229443376423d00d72e61e2e6f82ea5e0b2
Zachary Zumbo <zumbo@mit.edu>
2020-02-09 08:09:31 -0800

    Added toupcam driver and python wrapper for x64 Linux

---
+++ utils/qt.py
@@ -0,0 +1,90 @@
+import sys, toupcam
+from PyQt5.QtCore import pyqtSignal, pyqtSlot, Qt
+from PyQt5.QtGui import QPixmap, QImage
+from PyQt5.QtWidgets import QLabel, QApplication, QWidget, QDesktopWidget, QCheckBox, QMessageBox
+
+class MainWin(QWidget):
+    eventImage = pyqtSignal()
+
+    def __init__(self):
+        super().__init__()
+        self.hcam = None
+        self.buf = None      # video buffer
+        self.w = 0           # video width
+        self.h = 0           # video height
+        self.total = 0
+        self.setFixedSize(800, 600)
+        qtRectangle = self.frameGeometry()
+        centerPoint = QDesktopWidget().availableGeometry().center()
+        qtRectangle.moveCenter(centerPoint)
+        self.move(qtRectangle.topLeft())
+        self.initUI()
+        self.initCamera()
+
+    def initUI(self):
+        self.cb = QCheckBox('Auto Exposure', self)
+        self.cb.stateChanged.connect(self.changeAutoExposure)
+        self.label = QLabel(self)
+        self.label.setScaledContents(True)
+        self.label.move(0, 30)
+        self.label.resize(self.geometry().width(), self.geometry().height())
+
+# the vast majority of callbacks come from toupcam.dll/so/dylib internal threads, so we use qt signal to post this event to the UI thread  
+    @staticmethod
+    def cameraCallback(nEvent, ctx):
+        if nEvent == toupcam.TOUPCAM_EVENT_IMAGE:
+            ctx.eventImage.emit()
+
+# run in the UI thread
+    @pyqtSlot()
+    def eventImageSignal(self):
+        if self.hcam is not None:
+            try:
+                self.hcam.PullImageV2(self.buf, 24, None)
+                self.total += 1
+            except toupcam.HRESULTException:
+                QMessageBox.warning(self, '', 'pull image failed', QMessageBox.Ok)
+            else:
+                self.setWindowTitle('{}: {}'.format(self.camname, self.total))
+                img = QImage(self.buf, self.w, self.h, (self.w * 24 + 31) // 32 * 4, QImage.Format_RGB888)
+                self.label.setPixmap(QPixmap.fromImage(img))
+
+    def initCamera(self):
+        a = toupcam.Toupcam.EnumV2()
+        if len(a) <= 0:
+            self.setWindowTitle('No camera found')
+            self.cb.setEnabled(False)
+        else:
+            self.camname = a[0].displayname
+            self.setWindowTitle(self.camname)
+            self.eventImage.connect(self.eventImageSignal)
+            try:
+                self.hcam = toupcam.Toupcam.Open(a[0].id)
+            except toupcam.HRESULTException:
+                QMessageBox.warning(self, '', 'failed to open camera', QMessageBox.Ok)
+            else:
+                self.w, self.h = self.hcam.get_Size()
+                bufsize = ((self.w * 24 + 31) // 32 * 4) * self.h
+                self.buf = bytes(bufsize)
+                self.cb.setChecked(self.hcam.get_AutoExpoEnable())            
+                try:
+                    if sys.platform == 'win32':
+                        self.hcam.put_Option(toupcam.TOUPCAM_OPTION_BYTEORDER, 0) # QImage.Format_RGB888
+                    self.hcam.StartPullModeWithCallback(self.cameraCallback, self)
+                except toupcam.HRESULTException:
+                    QMessageBox.warning(self, '', 'failed to start camera', QMessageBox.Ok)
+
+    def changeAutoExposure(self, state):
+        if self.hcam is not None:
+            self.hcam.put_AutoExpoEnable(state == Qt.Checked)
+
+    def closeEvent(self, event):
+        if self.hcam is not None:
+            self.hcam.Close()
+            self.hcam = None
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    win = MainWin()
+    win.show()
+    sys.exit(app.exec_())
---
+++ utils/simplest.py
@@ -0,0 +1,56 @@
+import toupcam
+
+class App:
+    def __init__(self):
+        self.hcam = None
+        self.buf = None
+        self.total = 0
+
+# the vast majority of callbacks come from toupcam.dll/so/dylib internal threads
+    @staticmethod
+    def cameraCallback(nEvent, ctx):
+        if nEvent == toupcam.TOUPCAM_EVENT_IMAGE:
+            ctx.CameraCallback(nEvent)
+
+    def CameraCallback(self, nEvent):
+        if nEvent == toupcam.TOUPCAM_EVENT_IMAGE:
+            try:
+                self.hcam.PullImageV2(self.buf, 24, None)
+                self.total += 1
+                print('pull image ok, total = {}'.format(self.total))
+            except toupcam.HRESULTException:
+                print('pull image failed')
+        else:
+            print('event callback: {}'.format(nEvent))
+
+    def run(self):
+        a = toupcam.Toupcam.EnumV2()
+        if len(a) > 0:
+            print('{}: flag = {:#x}, preview = {}, still = {}'.format(a[0].displayname, a[0].model.flag, a[0].model.preview, a[0].model.still))
+            for r in a[0].model.res:
+                print('\t = [{} x {}]'.format(r.width, r.height))
+            self.hcam = toupcam.Toupcam.Open(a[0].id)
+            if self.hcam:
+                try:
+                    width, height = self.hcam.get_Size()
+                    bufsize = ((width * 24 + 31) // 32 * 4) * height
+                    print('image size: {} x {}, bufsize = {}'.format(width, height, bufsize))
+                    self.buf = bytes(bufsize)
+                    if self.buf:
+                        try:
+                            self.hcam.StartPullModeWithCallback(self.cameraCallback, self)
+                        except toupcam.HRESULTException:
+                            print('failed to start camera')
+                    input('press ENTER to exit')
+                finally:
+                    self.hcam.Close()
+                    self.hcam = None
+                    self.buf = None
+            else:
+                print('failed to open camera')
+        else:
+            print('no camera found')
+
+if __name__ == '__main__':
+    app = App()
+    app.run()
